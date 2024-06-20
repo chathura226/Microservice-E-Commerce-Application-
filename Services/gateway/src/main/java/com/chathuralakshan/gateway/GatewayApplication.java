b43d9b7983ff6751391c5f6863701901c3b9df1a
chathura226 <chathuralakshan226@gmail.com>
2024-06-20 13:33:02 +0530

    finished and tested wihtout security

---
+++ Services/gateway/src/main/java/com/chathuralakshan/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.chathuralakshan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
